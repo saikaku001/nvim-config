6bfb7f833a86df726168ffa4f39b26a8bf316c56
Saikaku Lab <130112558+saikaku001@users.noreply.github.com>
2025-12-02 23:04:40 +0900

    Add files via upload

--- init.lua
+++ init.lua
@@ -20,9 +20,10 @@ vim.opt.rtp:prepend(lazypath)
 
 -- 3. lazy.nvim のセットアップ
 require('lazy').setup(
-    -- lua/plugins/init.lua が返すプラグインのテーブルを渡す
-    require('plugins'),
     {
+	spec = {
+	    { import = "plugins" },
+	},
         -- lazy.nvim のオプション (例: デバッグ、UIなど)
         change_detection = {
             notify = false, -- 設定変更時の通知をオフにする
